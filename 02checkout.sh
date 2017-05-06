aa668a288bc1a45ca87a20c98340f769172cd59d
Anatoli Babenia <anatoli@rainforce.org>
2017-05-06 17:12:48 +0300

    Cleanup /var/www/html which is a new root

--- 02checkout.sh
+++ 02checkout.sh
@@ -1,3 +1,7 @@
 echo --- checkout sources into /var/www ---
-rm -rf /var/www/html/*
+rm -rf /var/www/html/.*
 git clone --depth=1 https://github.com/sergejey/majordomo.git /var/www/html
+
+echo --- creating config.php ---
+cd /var/www/html/
+cp config.php.sample config.php
